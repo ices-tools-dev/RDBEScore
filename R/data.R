4e9f3796713c5235cfed98911981309b48d18545
Richard Meitern <rix133@ut.ee>
2023-10-31 09:09:26 +0200

    remove obsolete files #163

--- R/data.R
+++ R/data.R
@@ -14,7 +14,7 @@
 #' @source \url{https://github.com/davidcurrie2001/MI_RDBES_ExchangeFiles}
 "tablesInRDBESHierarchies"
 
-#' A dataset containing test RDBES data for H1 in the DBErawObj structure
+#' A dataset containing test RDBES data for H1 in the RDBESDataObject structure
 #'
 #' @format A list containing entries required for H1 RDBES data:
 #' \describe{
@@ -31,28 +31,8 @@
 #'   \item{SL}{the Species List data frame}
 #'   ...
 #' }
-#' @source \url{https://github.com/davidcurrie2001/MI_RDBES_ExchangeFiles}
-"h1DBErawObj"
+"MadeUpData_for_SL_SA_tests_v1_WGRDBES_EST_TEST_3"
 
-#' A dataset containing test RDBES data for H1
-#'
-#' @format A list containing entries required for H1 RDBES data:
-#' \describe{
-#'   \item{DE}{the Design data frame}
-#'   \item{SD}{the Sampling Details data frame}
-#'   \item{VS}{the Vessel Selection data frame}
-#'   \item{FT}{the Fishing Trip data frame}
-#'   \item{FO}{the Fishing Operation data frame}
-#'   \item{SS}{the Species Selection data frame}
-#'   \item{SA}{the Sample data frame}
-#'   \item{FM}{the Frequency Measure data frame}
-#'   \item{BV}{the Biological Variable data frame}
-#'   \item{VD}{the Vessel Details data frame}
-#'   \item{SL}{the Species List data frame}
-#'   ...
-#' }
-#' @source \url{https://github.com/davidcurrie2001/MI_RDBES_ExchangeFiles}
-"h1Example"
 
 #' A dataset containing the mapping from database column names
 #' to R field names
