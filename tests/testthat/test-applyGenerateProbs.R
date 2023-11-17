f5db79236f7dad55949705b80fc01bcf72df8bb4
nmprista <31890653+nmprista@users.noreply.github.com>
2023-11-17 11:22:02 +0100

    fixed test on stratification added test on clustering related to #187

--- tests/testthat/test-applyGenerateProbs.R
+++ tests/testthat/test-applyGenerateProbs.R
@@ -4,12 +4,25 @@ capture.output({  ## suppresses printing of console output when running test()
     myPath <- "./h1_v_1_19_18"
     myObject <- importRDBESDataCSV(rdbesExtractPath = myPath)
     # Only use the non-clustered test data
-    myObject <- filterRDBESDataObject(myObject,c("DEstratumName"),c("DE_stratum1_H1","DE_stratum2_H1","DE_stratum3_H1"))
-    myObject <- findAndKillOrphans(myObject, verbose = FALSE)
+    myObject <- filterRDBESDataObject(myObject,c("DEstratumName"),c("DE_stratum1_H1"), killOrphans = T)
+    myObject[["FM"]]<-myObject[["FM"]][0,]
+    myObject[["BV"]]<-myObject[["BV"]][0,]
     myObject[["SA"]]$SAlowHierarchy <- "D"
     myObject[["VS"]][myObject[["VS"]]$VSstratumName == "VS_stratum2","VSnumTotal"] <- 40
-    expect_error(applyGenerateProbs(myObject, "selection"),NA)
 
+    expect_error(applyGenerateProbs(myObject, "selection"),NA)
   })
 
+  test_that("applyGenerateProbs stops errors when clustering is present",  {
+    myPath <- "./h1_v_1_19_18"
+    myObject <- importRDBESDataCSV(rdbesExtractPath = myPath)
+    # Only use the non-clustered test data
+    myObject <- filterRDBESDataObject(myObject,c("DEstratumName"),c("Pckg_SDAResources_coots_H1"), killOrphans = T)
+    myObject[["FM"]]<-myObject[["FM"]][0,]
+    myObject[["BV"]]<-myObject[["BV"]][0,]
+    myObject[["SA"]]$SAlowHierarchy <- "D"
+
+    myObject$VS$VSclustering
+    expect_error(applyGenerateProbs(myObject, "selection"),"clusters present: not yet specified")
 })
+}) ## end capture.output
