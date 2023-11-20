664a41a44cefcc17675e449668286ff221b32ad2
Kasia-MIR <60775640+Kasia-MIR@users.noreply.github.com>
2023-11-20 11:05:43 +0100

    vignette renameSA

--- WGRDBES-EST/personal/Kasia/renameSpeciesSA.R
+++ WGRDBES-EST/personal/Kasia/renameSpeciesSA.R
@@ -1,21 +1,33 @@
 #'Function which rename SAspeCode
 #'
 #' @param RDBESDataObject An RDBESDataObject.
+#' @param validate Set to TRUE if you want validation to be carried out. The
+#' default if TRUE.
+#' @param verbose (Optional) Set to TRUE if you want informative text on validation printed
+#' out, or FALSE if you don't.  The default is FALSE.
+#' @param strict (Optional) This function can validate its input data - should
+#' the validation be strict? The default is TRUE.
 #'
-#' @return RDBES data object where species in SA were renaming for species not
+#' @return RDBES data object where species in SA were renaming for species
 #' occuring in SL for level of species rank. If in SA is
 #' Sprat(126425), in SL Clupeidae (125464) function renameSpeciesSA rename Sprat
 #' from SA to Clupeidae. Clupeidae(family rank) is higher rank than Sprat(species rank).
 #'
 #' @example
 #' \dontrun{
-#' myObject <- createRDBESDataObject(input = "WGRDBES-EST/personal/Kasia/one_example")
-#' renameSpeciesSA(RDBESDataObject=myObject)}
+#' myObject <- createRDBESDataObject(input = "WGRDBES-EST/personal/Kasia/vignettes/vignetteData")
+#' renameSpeciesSA(RDBESDataObject=myObject,validate,verbose,strict)}
 
-renameSpeciesSA<-function(RDBESDataObject){
+renameSpeciesSA<-function(RDBESDataObject, validate=TRUE, verbose=FALSE, strict=TRUE){
+  if(validate){
+    validateRDBESDataObject(RDBESDataObject,
+                            verbose = verbose,
+                            strict = strict
+    )
+    }
 
 H1_SA <- RDBESDataObject[["SA"]]
-H1_SL <- RDBESDataObject[["SL"]]
+H1_SL <- fixSLids(RDBESDataObject)$SL
 H1_SS <- RDBESDataObject[["SS"]]
 
 aphiaRecords <- RDBEScore::wormsAphiaRecord
@@ -37,23 +49,22 @@ H1_SL_new <- dplyr::left_join(H1_SL,
 
 
 #SS only information about SLspecieslistName
-H1_SS<-H1_SS[,c('SSid','SSspecListName')]
+H1_SS<-H1_SS[,c('SSid','SLid','SSspecListName','SScatchFra')]
 H1_SA_new<-merge(H1_SA_new,H1_SS, by='SSid')
 
 #key of species list name and species
 
-H1_SA_new$SAkey<-paste(H1_SA_new$SAspeCode,H1_SA_new$SSspecListName,sep='_')
-H1_SL_new$SLkey<-paste(H1_SL_new$SLsppCode,H1_SL_new$SLspeclistName,sep='_')
+H1_SA_new$SAkey<-paste(H1_SA_new$SLid,H1_SA_new$SAspeCode,H1_SA_new$SSspecListName,H1_SA_new$SScatchFra,sep='_')
+H1_SL_new$SLkey<-paste(H1_SL_new$SLid,H1_SL_new$SLsppCode,H1_SL_new$SLspeclistName,H1_SL_new$SLcatchFrac,sep='_')
 
 newest_SA<-data.frame(NULL)
 
 unique(H1_SA_new$SSid)->sequence_SSid
-unique(H1_SA_new$SAcatchCat)->sequence_catchCategory
+#unique(H1_SA_new$SAcatchCat)->sequence_catchCategory
 
 for (k in sequence_SSid){
-  for (l in sequence_catchCategory){
     # subset of all SA by SSid for all catch category!!!
-    temp<-H1_SA_new[H1_SA_new$SSid==k & H1_SA_new$SAcatchCat==l,]
+    temp<-H1_SA_new[H1_SA_new$SSid==k,]
     temp$SAnewSpeciesCode <- NA
     temp$SAnewSpeciesCode <- as.character( temp$SAnewSpeciesCode)
     #check correct list of Species
@@ -90,11 +101,13 @@ for (k in sequence_SSid){
       }#i
       newest_SA<-rbind(newest_SA,temp)
     }
-  }#l
+
 }#k
 #rename SAspeCode
 newest_SA$SAspeCode<-newest_SA$SAnewSpeciesCode
-newest_SA<-data.table::setDT(newest_SA[,1:54])
+x<-colnames(H1_SA)
+newest_SA<-data.table::setDT(newest_SA[,..x])
 RDBESDataObject[["SA"]] <- newest_SA
+setkey(RDBESDataObject[["SA"]],"SAid")
 RDBESDataObject
 }
