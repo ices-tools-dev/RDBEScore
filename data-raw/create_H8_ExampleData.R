ddc411cd477cba939827144119a51a02f686a25f
Richard Meitern <richard.meitern@gmail.com>
2024-10-14 14:29:56 +0300

    update roxygen2 fixes #198

--- data-raw/create_H8_ExampleData.R
+++ data-raw/create_H8_ExampleData.R
@@ -3,6 +3,9 @@ ddir <- "./data-raw/exampleData/"
 
 #this data is created by Richard Meitern (contact: richard.meitern@ut.ee) internally
 H8ExampleEE1 <- readRDS(paste0(ddir, "H8_2023_10_31.rds"))
+#update from version 1.19.18 to 1.19.26
+H8ExampleEE1$CL$CLlanVal <- H8ExampleEE1$CL$CLtotOffLanVal
+H8ExampleEE1$CL$CLtotOffLanVal <- NULL
 validateRDBESDataObject(H8ExampleEE1)
 
 # Save the data
