7a20788b6dbcc8495211d8a7dc2edf63517278ea
David Currie <davidcurrie2001@yahoo.co.uk>
2024-10-16 13:42:43 +0100

    CHanged default value for killOrphans to True, and added a call to removeBrokenSpeciesListLinks() function

--- R/filterAndTidyRDBESDataObject.R
+++ R/filterAndTidyRDBESDataObject.R
@@ -2,12 +2,12 @@
 #'
 #' This function filters an RDBESDataObject based on specified fields and values, and can optionally remove any orphan records.
 #' The returned object will include all rows which either: a) do not include any of the field names in `fieldsToFilter`, or b) do include the field names and have one of the allowed values in `valuesToFilter`.
-#' If `killOrphans` is set to `TRUE`, the function will remove orphaned rows. The default is `FALSE`.
+#' If `killOrphans` is set to `TRUE`, the function will remove orphaned rows. The default is `TRUE`.
 #'
 #' @param RDBESDataObjectToFilterAndTidy The RDBESDataObject to filter.
 #' @param fieldsToFilter A vector of the field names you wish to check.
 #' @param valuesToFilter A vector of the field values you wish to filter for.
-#' @param killOrphans Controls if orphan rows are removed. Default is `FALSE`.
+#' @param killOrphans Controls if orphan rows are removed. Default is TRUE.
 #' @param verbose (Optional) Set to TRUE if you want informative text printed
 #' out, or FALSE if you don't.  The default is FALSE.
 #' @param strict (Optional) This function validates its input data - should
@@ -38,7 +38,7 @@
 filterAndTidyRDBESDataObject <- function(RDBESDataObjectToFilterAndTidy,
                                  fieldsToFilter,
                                  valuesToFilter,
-                                 killOrphans = FALSE,
+                                 killOrphans = TRUE,
                                  verbose = FALSE,
                                  strict = TRUE)
   {
@@ -57,18 +57,22 @@ filterAndTidyRDBESDataObject <- function(RDBESDataObjectToFilterAndTidy,
                                                       valuesToFilter,
                                                       verbose = verbose,
                                                       strict = strict)
-  # 2 - remove broken vessels links, broken species links not included yet
-  # note to myself
-  if (any(grepl("VD", fieldsToFilter)))
-    print(paste0("VD filtered by: ", fieldsToFilter[which(grepl("VD", fieldsToFilter))]))
+  # 2 - remove broken vessels links
     RDBESDataObjectToFilterAndTidy <-
       removeBrokenVesselLinks(RDBESDataObjectToFilterAndTidy,
                               verbose = verbose,
                               strict = strict)
 
+  # 2a - remove any broken species list links
+    RDBESDataObjectToFilterAndTidy <-
+      removeBrokenSpeciesListLinks(RDBESDataObjectToFilterAndTidy,
+                              verbose = verbose,
+                              strict = strict)
+
+
 
   # 3 - remove orphans
-  # Remove orphans after filtering and removing data missed in VD
+  # Remove orphans after filtering
   if (killOrphans == TRUE)
       RDBESDataObjectToFilterAndTidy <-
       findAndKillOrphans(RDBESDataObjectToFilterAndTidy,
