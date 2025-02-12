501758fc6d2edd79bcee9d5d4860243024ffdf17
Richard Meitern <richard.meitern@gmail.com>
2025-02-12 10:32:53 +0200

    update H1 and H5 test data

--- data-raw/create_H8_ExampleData.R
+++ data-raw/create_H8_ExampleData.R
@@ -3,7 +3,7 @@ ddir <- "./data-raw/exampleData/"
 
 #this data is created by Richard Meitern (contact: richard.meitern@ut.ee) internally
 H8ExampleEE1 <- readRDS(paste0(ddir, "H8_2025_02_11.rds"))
-validateRDBESDataObject(H8ExampleEE1)
+RDBEScore::validateRDBESDataObject(H8ExampleEE1)
 
 # Save the data
 usethis::use_data(H8ExampleEE1, overwrite = TRUE)
