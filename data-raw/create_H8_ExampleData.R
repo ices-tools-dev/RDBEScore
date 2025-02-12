42fcfbb065852c129dbbc599846f603b781476c9
Richard Meitern <richard.meitern@gmail.com>
2025-02-12 09:54:30 +0200

    update H8 example data

--- data-raw/create_H8_ExampleData.R
+++ data-raw/create_H8_ExampleData.R
@@ -2,10 +2,7 @@
 ddir <- "./data-raw/exampleData/"
 
 #this data is created by Richard Meitern (contact: richard.meitern@ut.ee) internally
-H8ExampleEE1 <- readRDS(paste0(ddir, "H8_2023_10_31.rds"))
-#update from version 1.19.18 to 1.19.26
-H8ExampleEE1$CL$CLlanVal <- H8ExampleEE1$CL$CLtotOffLanVal
-H8ExampleEE1$CL$CLtotOffLanVal <- NULL
+H8ExampleEE1 <- readRDS(paste0(ddir, "H8_2025_02_11.rds"))
 validateRDBESDataObject(H8ExampleEE1)
 
 # Save the data
