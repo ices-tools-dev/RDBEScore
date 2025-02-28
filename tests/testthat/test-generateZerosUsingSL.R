6e7e2738442e5c4819f38d03376561768b97ec61
David Currie <davidcurrie2001@yahoo.co.uk>
2025-02-28 16:37:38 +0000

    Tidied up tests #220

--- tests/testthat/test-generateZerosUsingSL.R
+++ tests/testthat/test-generateZerosUsingSL.R
@@ -16,31 +16,23 @@ createTestData <- function(){
 
   validateRDBESDataObject(myH1DataObject0, checkDataTypes = TRUE)
 
-  #df1 <- data.frame('31831','SL','ZW','4484',myH1DataObject0[["SL"]]$SLspeclistName,'1965','Dis','107254','107254')
   df1SL <- data.frame('31831','SL','ZW','4484',myH1DataObject0[["SL"]]$SLspeclistName,'1965','Dis')
   df1IS <- data.frame('31832','31831','IS','107254','107254')
 
-  #colnames(df1) <- names(myH1DataObject0[["SL"]])
-  #colnames(df1SL) <- names(myH1DataObject0[["SL"]])
   data.table::setnames(df1SL, names(myH1DataObject0[["SL"]]))
-  #colnames(df1IS) <- names(myH1DataObject0[["IS"]])
   data.table::setnames(df1IS, names(myH1DataObject0[["IS"]]))
-  #myH1DataObject0[["SL"]] <- rbind(myH1DataObject0[["SL"]],df1)
   myH1DataObject0[["SL"]] <- rbind(myH1DataObject0[["SL"]],df1SL)
   myH1DataObject0[["SL"]]$SLid <- as.integer(myH1DataObject0[["SL"]]$SLid)
   myH1DataObject0[["SL"]]$SLyear <- as.integer(myH1DataObject0[["SL"]]$SLyear)
   myH1DataObject0[["IS"]] <- rbind(myH1DataObject0[["IS"]],df1IS)
   myH1DataObject0[["IS"]]$ISid <- as.integer(myH1DataObject0[["IS"]]$ISid)
   myH1DataObject0[["IS"]]$SLid <- as.integer(myH1DataObject0[["IS"]]$SLid)
-  #myH1DataObject0[["SL"]]$SLcommTaxon <- as.integer(myH1DataObject0[["SL"]]$SLcommTaxon)
-  #myH1DataObject0[["SL"]]$SLsppCode <- as.integer(myH1DataObject0[["SL"]]$SLsppCode)
   myH1DataObject0[["IS"]]$IScommTaxon <- as.integer(myH1DataObject0[["IS"]]$IScommTaxon)
   myH1DataObject0[["IS"]]$ISsppCode <- as.integer(myH1DataObject0[["IS"]]$ISsppCode)
   # add an additional species list - could be many in the SL download
   extraSL <- myH1DataObject[["SL"]][1,]
   extraSL$SLid <- as.integer(max(myH1DataObject[["SL"]]$SLid) +1)
   myH1DataObject0[["SL"]]<-rbind(myH1DataObject0[["SL"]], extraSL)
-  #myH1DataObject0[["SL"]]<-rbind(myH1DataObject0[["SL"]], myH1DataObject[["SL"]][1,])
 
   # ensure key is set on SL and IS
   setkey(myH1DataObject0[["SL"]], SLid)
@@ -75,7 +67,7 @@ test_that("generateZerosUsingSL creates rows for SLcou*SLinst*SLspeclistName*SLy
 	  expectSARowsFromSS <- 1
 	  expectSARowsFromSL <- 1
 
-		myTest <- generateZerosUsingSL(myH1DataObject1)
+		myTest <- generateZerosUsingSL(myH1DataObject1, verbose = TRUE)
 
 	  # create aux id_table [Nuno's function] and tmpKey to use in test
 		#aux<-createTableOfRDBESIds(x = myTest3, addSAseqNums=FALSE)
@@ -230,6 +222,8 @@ test_that("SScatchFra=='Catch' and spp absent", {
   # Get rid of the IS rows that contain species not in SA
   spToKeep <- myH1DataObject1[["SA"]]$SAspeCode
   myH1DataObject1[["IS"]] <- myH1DataObject1[["IS"]][myH1DataObject1[["IS"]]$IScommTaxon == spToKeep,]
+  # Make sure the SLid in IS is the one referred to by the SS data
+  myH1DataObject1[["IS"]]$SLid <- min(myH1DataObject1[["SS"]]$SLid)
   # Add a species that is not in SA
   tmpIS <- myH1DataObject1[["IS"]]
   tmpIS$ISid <- tmpIS$ISid +1
@@ -261,143 +255,81 @@ test_that("SScatchFra=='Catch' and spp absent", {
 
 })
 
-test_that("Produces errors as expected", {
+test_that("Produces error when there are multiple values of SAsex per SSid*SAstratumName", {
 
   # issue error if >1 SAcatchCat OR SAsex OR SAlandCat in same SSid*SAstratumName
   # explanation:
   # in a normal situation these variables are filled in the new from the 1st row of SSid*SAstratumName
   # if it happens that there are >1 values in that SSid*SAstratumName this is not valid - an error is issued
-  #myH1DataObject2<-myH1DataObject0
-  #myH1DataObject2$SA$SSid[1]<-myH1DataObject2$SA$SSid[2]
-  #myH1DataObject2$SA$SAcatchCat[1]<-"Dis"
-  #expect_error(generateZerosUsingSL(myH1DataObject2))
 
-  #myH1DataObject2<-myH1DataObject0
-  #myH1DataObject2$SA$SSid[1]<-myH1DataObject2$SA$SSid[2]
-  #myH1DataObject2$SA$SAsex[1]<-"F"
-  #expect_error(generateZerosUsingSL(myH1DataObject2))
+  myH1DataObject1 <- createTestData()
 
-  #myH1DataObject2<-myH1DataObject0
-  #myH1DataObject2$SA$SSid[1]<-myH1DataObject2$SA$SSid[2]
-  #myH1DataObject2$SA$SAlandCat[1]<-"HuC"
-  #expect_error(generateZerosUsingSL(myH1DataObject2))
+  # Make the SA rows stratified
+  myH1DataObject1[["SA"]]$SAstratification <- "Y"
+  myH1DataObject1[["SA"]]$SAstratumName <- "S1"
+  myH1DataObject1[["SA"]]$SAsex <- "M"
+  # Duplicate the SA row and change the sex
+  tmpSA <- myH1DataObject1[["SA"]][1,]
+  tmpSA$SAid <- tmpSA$SAid + 1
+  tmpSA$SAsex <- "F"
+  myH1DataObject1[["SA"]] <- rbind(myH1DataObject1[["SA"]], tmpSA)
+  setkey(myH1DataObject1[["SA"]], "SAid")
 
-
-# dev notes
-# tests should be partioned into several test_that sections
-
-#myH1DataObject1[c("SL","SS", "SA")]
-#generateZerosUsingSL(myH1DataObject1)[c("SL","SS", "SA")]
-
-# species*catchFrac in SL and not in SA: expected behavior -> generate a 0 row in SA
-# also tests creation of species list when SS present and no SA (e.g., 0 discards)
-
-# 		myTest3$SA$SLid <- myTest3$SS$SLid[match(aux$SSid[match(myTest3$SS$SSid,aux$SSid)], myTest3$SS$SSid)]
-# 		myTest3$SL <- myTest3$SL[myTest3$SL$SLid %in% myTest3$SA$SLid, ]
-#
-# 	#run tests
-# 	# all species in species list are now present in SA
-# 		expect_equal(all(paste0(myTest3$SL$SLid, myTest3$SL$SLcommTaxon) %in% paste0(myTest3$SA$SLid, myTest3$SA$SAspeCode)),TRUE)
-# 	# right number of rows generated
-# 		expect_equal(nrow(myTest3$SA),2)
-# 	# adds 1 more species to SL to test
-# 		myH1DataObject2<-myH1DataObject1
-# 		myH1DataObject2$SL<-rbind(myH1DataObject2$SL[1,],myH1DataObject2$SL[1,],myH1DataObject2$SL)
-# 		myH1DataObject2$SL$SLid[1]<-31830 # dummy
-# 		myH1DataObject2$SL$SLid[2]<-31829 # dummy
-# 		myH1DataObject2$SL$SLcommTaxon[1:2]<-126437 # dummy
-# 		myH1DataObject2$SL$SLcatchFrac[1]<-"Lan" # dummy
-# 		myH1DataObject2$SL$SLsppCode[1:2]<-126437 # dummy
-# 		setkey(myH1DataObject2[["SL"]], SLid)
-# 		expect_equal(nrow(generateZerosUsingSL(myH1DataObject2)$SA),4)
-#
-# 	# no change if SSuseCalcZero<-'N'
-# 		myH1DataObject2<-myH1DataObject1
-# 		myH1DataObject2[["SS"]]$SSuseCalcZero<-'N'
-# 		expect_equal(generateZerosUsingSL(myH1DataObject2),myH1DataObject2)
-# 	#
-#
-# # species*catchFrac in SL and in SA: expected behavior -> do not generate a 0 row in SA
-#
-# 	myH1DataObject2<-myH1DataObject1
-# 	myH1DataObject2[["SS"]]<-myH1DataObject2[["SS"]][1,]
-# 	myH1DataObject2<-filterRDBESDataObject(myH1DataObject2, c("SLid"), myH1DataObject2[["SS"]]$SLid, killOrphans = TRUE, verbose = TRUE)
-#
-# 	validateRDBESDataObject(myH1DataObject2, checkDataTypes = TRUE)
-#
-# 	result <- generateZerosUsingSL(myH1DataObject2)
-#
-# 	expect_equal(result, myH1DataObject2)
-#
-# # if SScatchFra=="Catch" it should not generate 0s for any SA strata if spp present in list
-#
-# 	myH1DataObject2<-myH1DataObject1
-# 		myH1DataObject2[["SS"]]<-myH1DataObject2[["SS"]][1,]
-# 		myH1DataObject2<-filterRDBESDataObject(myH1DataObject2, c("SLid"), myH1DataObject2[["SS"]]$SLid, killOrphans = TRUE)
-# 		myH1DataObject2[["SS"]]$SScatchFra<-"Catch"
-# 		myH1DataObject2[["SL"]]$SLcatchFrac<-"Catch"
-# 		# creates stratification in SA
-# 		myH1DataObject2[["SA"]]<-rbind(myH1DataObject2[["SA"]][1,],myH1DataObject2[["SA"]][1,])
-# 		myH1DataObject2[["SA"]]$SAstratification<-"Y"
-# 		myH1DataObject2[["SA"]]$SAid[2]<-myH1DataObject2[["SA"]]$SAid[1]+1
-# 		myH1DataObject2[["SA"]]$SAstratumName<-c("Landings","Discards")
-# 		myH1DataObject2[["SA"]]$SAcatchCat<-c("Lan","Dis")	# this is just cosmetics
-# 		setkey(myH1DataObject2[["SA"]], "SAid")
-#
-# 	validateRDBESDataObject(myH1DataObject2, checkDataTypes = TRUE)
-#
-# 	result <- generateZerosUsingSL(myH1DataObject2)
-#
-# 	expect_equal(result, myH1DataObject2)
-#
-# # if SScatchFra=="Catch" it should generate 0s for all SA strata present if spp absent(but in SL)
-#
-# 	myH1DataObject2<-myH1DataObject1
-# 		myH1DataObject2[["SS"]]<-myH1DataObject2[["SS"]][1,]
-# 		myH1DataObject2<-filterRDBESDataObject(myH1DataObject2, c("SLid"), myH1DataObject2[["SS"]]$SLid, killOrphans = TRUE)
-# 		myH1DataObject2[["SS"]]$SScatchFra<-"Catch"
-# 		myH1DataObject2[["SL"]]$SLcatchFrac<-"Catch"
-#
-# 		# use in debug
-# 		#myH1DataObject2[c(10,11,15)]
-#
-# 		# creates stratification in SA
-# 		myH1DataObject2[["SA"]]<-rbind(myH1DataObject2[["SA"]][1,],myH1DataObject2[["SA"]][1,])
-# 		myH1DataObject2[["SA"]]$SAstratification<-"Y"
-# 		myH1DataObject2[["SA"]]$SAid[2]<-myH1DataObject2[["SA"]]$SAid[1]+1
-# 		myH1DataObject2[["SA"]]$SAstratumName<-c("Landings","Discards")
-# 		myH1DataObject2[["SA"]]$SAcatchCat<-c("Lan","Dis")	# this is just cosmetics
-# 		setkey(myH1DataObject2[["SA"]], "SAid")
-# 		# creates a spp not existing in SA
-# 		myH1DataObject2[["SL"]]$SLcommTaxon<-as.integer(127007)
-#
-# 	validateRDBESDataObject(myH1DataObject2, checkDataTypes = TRUE)
-#
-# 	result <- generateZerosUsingSL(myH1DataObject2)
-#
-# 	expect_equal(nrow(result$SA),4)
-#
-# # issue error if >1 SAcatchCat OR SAsex OR SAlandCat in same SSid*SAstratumName
-# 	# explanation:
-# 		# in a normal situation these variables are filled in the new from the 1st row of SSid*SAstratumName
-# 		# if it happens that there are >1 values in that SSid*SAstratumName this is not valid - an error is issued
-# 	myH1DataObject2<-myH1DataObject0
-# 	myH1DataObject2$SA$SSid[1]<-myH1DataObject2$SA$SSid[2]
-# 	myH1DataObject2$SA$SAcatchCat[1]<-"Dis"
-# 	expect_error(generateZerosUsingSL(myH1DataObject2))
-#
-# 	myH1DataObject2<-myH1DataObject0
-# 	myH1DataObject2$SA$SSid[1]<-myH1DataObject2$SA$SSid[2]
-# 	myH1DataObject2$SA$SAsex[1]<-"F"
-# 	expect_error(generateZerosUsingSL(myH1DataObject2))
-#
-# 	myH1DataObject2<-myH1DataObject0
-# 	myH1DataObject2$SA$SSid[1]<-myH1DataObject2$SA$SSid[2]
-# 	myH1DataObject2$SA$SAlandCat[1]<-"HuC"
-# 	expect_error(generateZerosUsingSL(myH1DataObject2))
-
+  expect_error(generateZerosUsingSL(myH1DataObject1),
+               "Cannot generateZerosUsingSL*")
 
 })
+
+test_that("Produces error when there are multiple values of SAcatchCat per SSid*SAstratumName", {
+
+  # issue error if >1 SAcatchCat OR SAsex OR SAlandCat in same SSid*SAstratumName
+  # explanation:
+  # in a normal situation these variables are filled in the new from the 1st row of SSid*SAstratumName
+  # if it happens that there are >1 values in that SSid*SAstratumName this is not valid - an error is issued
+
+  myH1DataObject1 <- createTestData()
+
+  # Make the SA rows stratified
+  myH1DataObject1[["SA"]]$SAstratification <- "Y"
+  myH1DataObject1[["SA"]]$SAstratumName <- "S1"
+  myH1DataObject1[["SA"]]$SAcatchCat <- "Lan"
+  # Duplicate the SA row and change the sex
+  tmpSA <- myH1DataObject1[["SA"]][1,]
+  tmpSA$SAid <- tmpSA$SAid + 1
+  tmpSA$SAcatchCat <- "Dis"
+  myH1DataObject1[["SA"]] <- rbind(myH1DataObject1[["SA"]], tmpSA)
+  setkey(myH1DataObject1[["SA"]], "SAid")
+
+  expect_error(generateZerosUsingSL(myH1DataObject1),
+               "Cannot generateZerosUsingSL*")
+
+})
+
+test_that("Produces error when there are multiple values of SAlandCat per SSid*SAstratumName", {
+
+  # issue error if >1 SAcatchCat OR SAsex OR SAlandCat in same SSid*SAstratumName
+  # explanation:
+  # in a normal situation these variables are filled in the new from the 1st row of SSid*SAstratumName
+  # if it happens that there are >1 values in that SSid*SAstratumName this is not valid - an error is issued
+
+  myH1DataObject1 <- createTestData()
+
+  # Make the SA rows stratified
+  myH1DataObject1[["SA"]]$SAstratification <- "Y"
+  myH1DataObject1[["SA"]]$SAstratumName <- "S1"
+  myH1DataObject1[["SA"]]$SAlandCat <- "Ind"
+  # Duplicate the SA row and change the sex
+  tmpSA <- myH1DataObject1[["SA"]][1,]
+  tmpSA$SAid <- tmpSA$SAid + 1
+  tmpSA$SAlandCat <- "HuC"
+  myH1DataObject1[["SA"]] <- rbind(myH1DataObject1[["SA"]], tmpSA)
+  setkey(myH1DataObject1[["SA"]], "SAid")
+
+  expect_error(generateZerosUsingSL(myH1DataObject1),
+               "Cannot generateZerosUsingSL*")
+
+})
+
 
 }) ## end capture.output
 
