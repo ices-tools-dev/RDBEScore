23a531b1c2112276b4057f2b19b6a1b9c4df2121
David Currie <davidcurrie2001@yahoo.co.uk>
2025-04-14 14:08:20 +0100

    Removed some rogue slashes

--- R/listPackageFunctions.R
+++ R/listPackageFunctions.R
@@ -1,12 +1,12 @@
 #' Extract Functions and Descriptions from an R Package
 #'
-#' This function extracts the list of functions contained within a specified R package and retrieves a brief description for each function from the package documentation. The description is obtained by parsing the Rd file associated with each function to extract the text from the \code{\title} field. In addition, the function determines whether each function is exported from the package by comparing against the package’s exported names.
+#' This function extracts the list of functions contained within a specified R package and retrieves a brief description for each function from the package documentation. The description is obtained by parsing the Rd file associated with each function to extract the text from the \code{title} field. In addition, the function determines whether each function is exported from the package by comparing against the package’s exported names.
 #'
 #' @param pkg A character string or an unquoted name specifying the package from which to extract functions. The package must be installed and accessible.
 #'
 #' @return A data frame with three columns. The \code{Function} column lists the names of the functions found in the package. The \code{Description} column contains the brief descriptions extracted from each function’s documentation, and the \code{Exported} column is a logical vector indicating whether the function is exported from the package.
 #'
-#' @details The function first accesses the package namespace using \code{asNamespace} and retrieves all objects using \code{ls}. It filters these objects to include only functions. For each function, the associated help file is retrieved using \code{utils::help} and the Rd file is extracted with \code{utils:::.getHelpFile}. The internal helper function \code{getRdTitle} is then used to parse the Rd object and extract the text in the \code{\title} field. Finally, the function assembles the output into a data frame that also includes an indicator of whether each function is exported.
+#' @details The function first accesses the package namespace using \code{asNamespace} and retrieves all objects using \code{ls}. It filters these objects to include only functions. For each function, the associated help file is retrieved using \code{utils::help} and the Rd file is extracted with \code{utils:::.getHelpFile}. The internal helper function \code{getRdTitle} is then used to parse the Rd object and extract the text in the \code{title} field. Finally, the function assembles the output into a data frame that also includes an indicator of whether each function is exported.
 #'
 #' @examples
 #' \dontrun{
@@ -42,7 +42,7 @@ listPackageFunctions <- function(pkg) {
 
 #' Extract Title from an Rd Object
 #'
-#' This internal helper function traverses an Rd object to extract the text from the \code{\title} field. It is used by \code{listPackageFunctions} to obtain a concise description for each documented function.
+#' This internal helper function traverses an Rd object to extract the text from the \code{title} field. It is used by \code{listPackageFunctions} to obtain a concise description for each documented function.
 #'
 #' @param rd An Rd object typically obtained via \code{utils:::.getHelpFile}.
 #'
