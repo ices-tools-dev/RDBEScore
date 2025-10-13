69ff5876cad1c02dc9f941019264efb21737c555
Richard Meitern <richard.meitern@ut.ee>
2025-10-13 21:31:13 +0300

    update H7 dataset to latest format

---
+++ data-raw/create_H7_ExampleData.R
@@ -0,0 +1,19 @@
+## Load the csv files from our data-raw
+ddir <- "./data-raw/exampleData/"
+
+#this data is created by Richard Meitern (contact: richard.meitern@ut.ee)
+# internally it resembles Estonian onshore sampling data
+H7Example <- readRDS(paste0(ddir, "H7_2025_10_13.rds"))
+
+H7Example$VD <- NULL
+H7Example$DE$DEsampScheme <- gsub("EST_","", H7Example$DE$DEsampScheme)
+H7Example$SL <- H7Example$SL[H7Example$SL$SLspeclistName %in%
+                               unique(H7Example$SS$SSspecListName),]
+#add 0 row VD
+H7Example$VD <- data.table::copy(H1Example$VD[F,])
+
+
+RDBEScore::validateRDBESDataObject(H7Example)
+H7ExampleEE1 <- H7Example
+# Save the data
+usethis::use_data(H7ExampleEE1, overwrite = TRUE)
