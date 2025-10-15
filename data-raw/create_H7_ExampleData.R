1b6546d38b47cdaca9b00756f601d094c1ae99b6
Richard Meitern <richard.meitern@ut.ee>
2025-10-15 11:41:54 +0300

    fixes H7 example #245

--- data-raw/create_H7_ExampleData.R
+++ data-raw/create_H7_ExampleData.R
@@ -9,11 +9,12 @@ H7Example$VD <- NULL
 H7Example$DE$DEsampScheme <- gsub("EST_","", H7Example$DE$DEsampScheme)
 H7Example$SL <- H7Example$SL[H7Example$SL$SLspeclistName %in%
                                unique(H7Example$SS$SSspecListName),]
+
+H7Example$IS <- H7Example$IS[H7Example$IS$SLid %in% H7Example$SL$SLid,]
 #add 0 row VD
 H7Example$VD <- data.table::copy(H1Example$VD[F,])
 
 
 RDBEScore::validateRDBESDataObject(H7Example)
-H7ExampleEE1 <- H7Example
 # Save the data
-usethis::use_data(H7ExampleEE1, overwrite = TRUE)
+usethis::use_data(H7Example, overwrite = TRUE)
