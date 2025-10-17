896c3545b17580dd8e78b2ce2e0304381502b6f8
nmprista <31890653+nmprista@users.noreply.github.com>
2025-10-17 14:54:58 +0200

    Update add2PackageDataWGRDBES-EST_TEST_1.R
    changes to data name moved to upload
    added a test to speed up check on motive behind warning issued by createRDBESDataObject (NAs in VSnumTotal)

--- data-raw/add2PackageDataWGRDBES-EST_TEST_1.R
+++ data-raw/add2PackageDataWGRDBES-EST_TEST_1.R
@@ -1,6 +1,6 @@
 #read the downloaded zip file
 #this file is downloaded from the RDBES the contents of this file is prepared in
-# the files in the directory exampleData/TextBookExamples and uploaded to RDBES
+#the files in the directory exampleData/TextBookExamples and uploaded to RDBES
 
 zipFname <- "./data-raw/exampleData/TextBookExamples/DownloadsFromRDBES/ZW_1965_WGRDBES-EST_TEST_1.zip"
 data <- RDBEScore::createRDBESDataObject(zipFname)
@@ -11,20 +11,25 @@ data <- RDBEScore::filterRDBESDataObject(data,
                                          fieldsToFilter = "DEsampScheme",
                                          valuesToFilter = samp_scheme,
                                          killOrphans = T)
+
+# dataset Pckg_SDAResources_coots_multistage_H1 has NA as VSnumTotal which
+# creates a warning message when data is printed,. The following code
+# checks that dataset is the only case, otherwise stops
+aux<-RDBEScore:::createTableOfRDBESIds(data)
+VSid_without_VSnumTotal<-unique(
+  aux[aux$DEid==data$DE$DEid[
+      data$DE$DEstratumName=="Pckg_SDAResources_coots_multistage_H1"],"VSid"])
+if(!nrow(data$VS[is.na(data$VS$VSnumTotal)])==length(VSid_without_VSnumTotal)){
+  stop()
+}
+
 datasetNames <- unique(data$DE$DEstratumName)
 
-#fix wrong named data these are from survey actually
-wrongDataSets <- c('Pckg_SDAResources_apiclus1_v2_H1',
-                    'Pckg_SDAResources_apiclus2_v2_H1')
 #add all stratums to package data
 for(dname in datasetNames){
   #fix the naming removing strange characters
   datasetName <- gsub("[[:punct:]]|[[:space:]]", "_",
                       paste0(dname))
-  #replace the name
-  if(dname %in% wrongDataSets){
-    datasetName <- sub("SDAResources", "survey", datasetName)
-    }
   deData <-  RDBEScore::filterRDBESDataObject(data,
                                               fieldsToFilter = "DEstratumName",
                                               valuesToFilter = dname,
